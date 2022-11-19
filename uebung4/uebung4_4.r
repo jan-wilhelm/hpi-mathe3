111d057b6f388232b8e67e28c20ef28f377a4636
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2022-11-19 15:41:50 +0100

    uebung 4 solutions

---
+++ uebung4/uebung4_4.r
@@ -0,0 +1,21 @@
+# plot data from "bewohner_grossdorf.csv"
+
+# read data
+setwd("~/Desktop/uni/Mathe3/uebung4")
+
+# bewohner
+bewohner <- read.csv("bewohner_grossdorf.csv", header=TRUE, sep=",")
+hist(bewohner$height, breaks = 20, main="Verteilung der Höhe", freq=FALSE)
+
+# Plotte verschiedene Normalverteilungen (Aufgabe 4.4c)
+curve(dnorm(x,160,10), add=TRUE,col="green")
+curve(dnorm(x,180,10), add=TRUE,col="red")
+curve(dnorm(x,180,20), add=TRUE,col="blue")
+
+# ulf
+ulf <- read.csv("ulfs_cafe.csv", header=TRUE, sep=",")
+barplot(ulf$occurence, names.arg = ulf$student_count)
+
+# read "s_bahn.csv" and plot it in a histogram
+s_bahn <- read.csv("s_bahn.csv", header=TRUE, sep=",")
+hist(s_bahn$waiting_time, 20, main="Verteilung der Verspätungen")
