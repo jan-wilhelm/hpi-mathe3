30bc303773a1b878b401f32ad25c87779235e9fe
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2022-11-19 15:42:31 +0100

    Update uebung4_4.r

--- uebung4/uebung4_4.r
+++ uebung4/uebung4_4.r
@@ -15,6 +15,12 @@ curve(dnorm(x,180,20), add=TRUE,col="blue")
 # ulf
 ulf <- read.csv("ulfs_cafe.csv", header=TRUE, sep=",")
 barplot(ulf$occurence, names.arg = ulf$student_count)
+# plotte Poisson Verteilungen
+barplot(ulf$occurence / 1200, names.arg = ulf$student_count)
+curve(dnorm(x,9,4), add=TRUE,col="green")
+curve(dnorm(x,14,4), add=TRUE,col="red")
+curve(dnorm(x,9,8), add=TRUE,col="blue")
+
 
 # read "s_bahn.csv" and plot it in a histogram
 s_bahn <- read.csv("s_bahn.csv", header=TRUE, sep=",")
