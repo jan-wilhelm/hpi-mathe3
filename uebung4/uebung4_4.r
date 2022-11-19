2c806528a870ab6463c0d47468f338a7e3d734d8
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2022-11-19 15:44:55 +0100

    Update uebung4_4.r

--- uebung4/uebung4_4.r
+++ uebung4/uebung4_4.r
@@ -1,27 +1,24 @@
-# plot data from "bewohner_grossdorf.csv"
-
 # read data
 setwd("~/Desktop/uni/Mathe3/uebung4")
 
 # bewohner
-bewohner <- read.csv("bewohner_grossdorf.csv", header=TRUE, sep=",")
-hist(bewohner$height, breaks = 20, main="Verteilung der Höhe", freq=FALSE)
+bewohner <- read.csv("bewohner_grossdorf.csv", header = TRUE, sep = ",")
+hist(bewohner$height, breaks = 20, main = "Verteilung der Höhe")
 
 # Plotte verschiedene Normalverteilungen (Aufgabe 4.4c)
-curve(dnorm(x,160,10), add=TRUE,col="green")
-curve(dnorm(x,180,10), add=TRUE,col="red")
-curve(dnorm(x,180,20), add=TRUE,col="blue")
+# freq=FALSE weil wir es normiert haben wollen auf Höhe der Dichtefunktion
+hist(bewohner$height, breaks = 20, main = "Verteilung der Höhe", freq = FALSE)
+curve(dnorm(x,160,10), add = TRUE, col = "green")
+curve(dnorm(x,180,10), add = TRUE, col = "red")
+curve(dnorm(x,180,20), add = TRUE, col = "blue")
 
-# ulf
+# Lade daten von ulf, plotte sie als Bar Plot
 ulf <- read.csv("ulfs_cafe.csv", header=TRUE, sep=",")
 barplot(ulf$occurence, names.arg = ulf$student_count)
-# plotte Poisson Verteilungen
-barplot(ulf$occurence / 1200, names.arg = ulf$student_count)
-curve(dnorm(x,9,4), add=TRUE,col="green")
-curve(dnorm(x,14,4), add=TRUE,col="red")
-curve(dnorm(x,9,8), add=TRUE,col="blue")
-
-
-# read "s_bahn.csv" and plot it in a histogram
-s_bahn <- read.csv("s_bahn.csv", header=TRUE, sep=",")
-hist(s_bahn$waiting_time, 20, main="Verteilung der Verspätungen")
+# plotte Poisson Verteilungen mit verschiedenen Paramtern
+# 1200 ist beliebig gewählter Skalierungsfaktor, um den Bar
+#  plot visuell auf "Höhe" der Poisson Verteilungen zu bringen
+barplot(ulf$occurence / 1200, names.arg = ulf$student_count) 
+curve(dnorm(x,9,4), add = TRUE, col = "green")
+curve(dnorm(x,14,4), add = TRUE, col = "red")
+curve(dnorm(x,9,8), add = TRUE, col = "blue")
